7f2d09d8eae9b0b62fec562d686981ebd5af3962
John Oblenda <joandong2@gmail.com>
2020-02-28 10:31:02 -0500

    mvps

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -4,15 +4,17 @@ import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
 
-const CommentSection = props => {
-  // Add state for the comments
-
-  return (
-    <div>
-      {/* map through the comments data and return the Comment component */}
-      <CommentInput />
-    </div>
-  );
+const CommentSection = (props) => {
+    // Add state for the comments
+    return (
+        <div>
+            {/* map through the comments data and return the Comment component */}
+            {props.comments.map((item) => {
+                return <Comment comment={item} />;
+            })}
+            <CommentInput />
+        </div>
+    );
 };
 
 export default CommentSection;
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,35 +1,18 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data
-import dummyData from "../../dummy-data.js";
-
-console.log(dummyData);
+import data from "../../dummy-data.js";
 
 const PostsPage = () => {
     // set up state for your data
     return (
         <div className="posts-container-wrapper">
             {/* map through data here to return a Post and pass data as props to Post */}
-            {dummyData.map((item) => {
-                //console.log(item.username);
-                // let post = {
-                //     username: item.username,
-                //     thumbnailUrl: item.thumbnailUrl,
-                //     src: item.imageUrl
-                // };
-
-                return (
-                    <Post
-                        post={{
-                            username: item.username,
-                            thumbnailUrl: item.thumbnailUrl,
-                            src: item.imageUrl
-                        }}
-                    />
-                );
+            {data.map((item) => {
+                return <Post post={item} />;
             })}
         </div>
     );
