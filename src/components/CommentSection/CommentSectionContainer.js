4cc041454a280f310dc95396044c010cf9f29583
John Oblenda <joandong2@gmail.com>
2020-02-28 19:58:14 -0500

    trying comments stretch

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -1,18 +1,39 @@
 // You will add code to this file
-import React from "react";
+import React, { useState } from "react";
 import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
 
 const CommentSection = (props) => {
     // Add state for the comments
+    const [comments, setComments] = useState(props.comments);
+    //const [comment, setComment] = useState("");
+    //console.log(comments);
+
+    // const changeHandler = (e) => {
+    //     setComment(e.target.value);
+    // };
+
+    // const submitHandler = (e) => {
+    //     //console.log(e.target.value);
+    //     //alert(comment);
+    //     setComments({ comments: [...comments, comment] });
+    //     e.preventDefault();
+    // };
+
+    //console.log(newComments);
+
     return (
         <div className="comment-wrapper">
             {/* map through the comments data and return the Comment component */}
-            {props.comments.map((item, i) => {
+            {comments.map((item, i) => {
                 return <Comment key={i} comment={item} />;
             })}
-            <CommentInput />
+            <CommentInput
+            // comment={comment}
+            // changeComment={changeHandler}
+            // submitComment={submitHandler}
+            />
         </div>
     );
 };
